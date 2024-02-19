ac774fed4bd2eeee05a3e54e4b0a0c4813842698
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-19 20:43:56 +0800

    Add files via upload

---
+++ Hypotenuse.cs
@@ -0,0 +1,20 @@
+﻿namespace Hypotenuse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter side A: ");
+            Double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            Double B = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Side C is equals to: " + Math.Sqrt((Math.Pow(A, 2))+(Math.Pow(B,2))));
+            
+
+            Console.ReadLine();
+        }
+    }
+}
