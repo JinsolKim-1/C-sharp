e349057bd7ee707cda64f119d3f47547acf0ef6a
kimji <kimji@LAPTOP-14U3RDKE>
2024-02-19 18:58:54 +0800

    Add project files.

---
+++ SimpleGreeting/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleGreeting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Type your name: " );
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Type your age: " );
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name + "." + "Your age is: " + age);
+            Console.WriteLine("\"WELCOME TO ALGOS\"");
+            Console.WriteLine("Press ANY key to exit");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
