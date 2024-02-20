cbb5ee038a9d4462075fa4fc490536789ab6e220
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-20 12:57:33 +0800

    Add files via upload

---
+++ Factroial.cs
@@ -0,0 +1,22 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO ALGOS");
+            Console.WriteLine("Please enter a non-negative integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Factorial of " + number + " is " + Factor(number));
+            Console.ReadLine();
+        }
+
+        private static long Factor(int n) 
+        {
+            return n == 0 ? 1 : (long)n * Factor(n - 1);
+
+            //can only handle upto 25 non-negative integers
+        }
+    }
+}
