9187f93ac10780f867b4995c5a56c391bd0ae08d
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-20 15:23:57 +0800

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace CalculatorProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "Addition";
+            String b = "Multiplication";
+            String c = "Subtraction";
+            String d = "Division";
+              
+            //User's input
+            Console.WriteLine("Input First number: ");
+            Double First = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("input Second number: ");
+            Double Second = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The following are the answers on each arithmetic operation: ");
+
+            Console.WriteLine(a);
+            Console.WriteLine(First + Second);
+
+            Console.WriteLine(b);
+            Console.WriteLine(First * Second);
+
+            Console.WriteLine(c);
+            Console.WriteLine(First - Second);
+
+            Console.WriteLine(d);
+            Console.WriteLine(First / Second);  
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
