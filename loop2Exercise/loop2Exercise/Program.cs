c59d5e8d8a673502ff7f73508907c957ac607a79
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-24 22:07:42 +0800

    Add files via upload

---
+++ loop2Exercise/loop2Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace loop2Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //count is set to 5
+            int count = 5;
+            //while count is greater than 0, then loop
+            while (count > 0)
+            {
+                count = count * 3;
+                //makes count less than 0 so it will not loop forever
+                count = count * -1;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }   
+        //ANSWER: Ended in one loop
+    }
+}
