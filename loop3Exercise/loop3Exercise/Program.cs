8d10f339a1683de4c52eefe249e6702cf114a952
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-24 22:26:50 +0800

    Add files via upload

---
+++ loop3Exercise/loop3Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace loop3Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 2;
+            // if i < 100, then i = 1 ; j - 1
+            // execute while loop
+            // after executing while loop, i will be multiplied by 2
+            for (int i = 1; i < 100; i = i * 2)
+                // int i = 1,2,4,8,16,32,64
+                //cannot be 128 since i will be greater than 100
+            {
+                j = j - 1;
+
+                //while j < 25, then add 5 
+                while (j < 25)
+                {
+                    j = j + 5;
+                    // j = 6,10,14,18,22,26
+                }
+                Console.WriteLine(j);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
