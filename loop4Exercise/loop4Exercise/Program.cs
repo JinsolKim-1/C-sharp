0e52b8a2c6b9660238e47ca93436dc7cf4f1d84b
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-24 22:36:36 +0800

    Add files via upload

---
+++ loop4Exercise/loop4Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace loop4Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int count = 2;
+            while (i == 0)
+            {
+                count = count * 3;
+                count = count * -1;
+
+                Console.WriteLine(count);
+            }
+            Console.ReadLine();
+        }       
+    }  //ANSWER: will run forever since i will be forever zero
+}
