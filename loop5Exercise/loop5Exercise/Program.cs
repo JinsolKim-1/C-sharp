bd1031ab7c817966dce9bd804de24b9bfcb82a2e
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2024-02-24 22:45:21 +0800

    Add files via upload

---
+++ loop5Exercise/loop5Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace loop5Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int i = 1;
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            //ANSWER: Infinite loop since i = 1 is always true
+        }
+    }
+}
