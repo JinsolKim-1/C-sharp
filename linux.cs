5f761a3ea280e210b0911b321ac308a98cbdaa48
JinsolKim-1 <atomicnick43@gmail.com>
2024-03-20 23:04:33 +0800

    from linux

---
+++ linux.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace linux
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Linux Community!");
+			Console.ReadLine();
+		}
+	}
+}
+			
+
